ed05af8e2c1d143ce67090a862b311246febf20f
anton-chornobai <antonchornobajj1@gmail.com>
2025-08-26 10:08:04 +0200

    done

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,8 +1,8 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
-        if (password.equals(repeatPassword) && password.length() >= 10) {
+    public void validate(String password, String rP) throws PasswordValidationException {
+        if (password.equals(rP) && password.length() >= 10) {
             throw new PasswordValidationException("Wrong passowrds");
         }
     }
